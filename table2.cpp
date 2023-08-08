9ecc665a2c3c025af7a4c34344651c0d19b7c4bb
md-hanif14 <hanifuzzaman.programminghero@gmail.com>
2023-08-08 18:16:05 +0600

    table 2

---
+++ table2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=2, b=3, c= 4, d=5;
+    for (int i=1; i<=10; i++)
+    {
+        cout<< a*i <<  b*i <<  c*i <<  d*i << endl;
+    }
+}
