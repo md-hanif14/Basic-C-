27da4530bf4a871aff579892984fa71193120c4c
md-hanif14 <hanifuzzaman.programminghero@gmail.com>
2023-08-08 16:58:13 +0600

    even odd

---
+++ even_odd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+     int a;
+     cout<< "Enter a number "<< endl;
+     cin>> a;
+
+     if(a%2==0)
+     {
+         cout<< "This is an even number"<< endl;
+     }
+     else{
+        cout<< "This is an odd number"<< endl;
+     }
+ }
